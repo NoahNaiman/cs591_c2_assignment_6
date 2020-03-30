d5ed207de039b66cba7eb35d26a699e24369e4df
Noah Naiman <noahnaiman@Noahs-MacBook-Pro.local>
2020-03-30 09:50:45 -0600

    Initial version passing first test

--- problem_1/problem1_tests.py
+++ problem_1/problem1_tests.py
@@ -1,7 +1,10 @@
 import pytest
+from url_splitter import url_split
 def test_empty_string():
     test_string = "https://www.google.com/some-path"
-    protocol, domain, path = url_splitter.split(test_string)
+    protocol, domain, path = url_split(test_string)
     assert protocol == "https"
     assert domain == "www.google.com"
     assert path == "some-path"
+
+test_empty_string()
---
+++ problem_1/url_splitter.py
@@ -0,0 +1,16 @@
+def url_split(url):
+    protocol = ""
+    domain = ""
+    path = ""
+
+    if url != "":
+        split_url = url.split("/")
+        split_url = [string for string in split_url if string != ""]
+        
+        protocol = split_url[0]
+        if protocol[-1] == ":":
+            protocol = protocol[:-1]
+        domain = split_url[1]
+        path = "/".join(split_url[2:])
+
+    return protocol, domain, path
